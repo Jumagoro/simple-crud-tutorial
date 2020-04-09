abd2db24ed62db1b377d58ef438ead55dbd80b4c
Gordian <gogoh60@gmail.com>
2020-04-09 09:07:11 +0200

    Added routing + components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,32 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ListBooksComponent } from './components/list-books/list-books.component';
+import { HomeComponent } from './components/home/home.component';
 
 
-const routes: Routes = [];
+// Wenn browser url: website.com/listBooks -> weiterleitung an ListBooksComponent
+const routes: Routes = [
+  {
+    path: "listBooks",
+    component: ListBooksComponent,
+  },
+  {
+    path: "home",
+    component: HomeComponent,
+  },
+  {
+    path: "**",
+    redirectTo: "/home"
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
+// Variable (let), Name, : [typ] = ...
+//let testVariable: string = "test";
+//let testVariable: string[] = ["test", ""];
+//let testVariable: number = 1;
